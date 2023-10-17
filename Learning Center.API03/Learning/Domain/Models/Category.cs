54fc90f11e7ae6af236ba25ae0e0d4169f5febd6
Jaime Escobar <pcccjesc@upc.edu.pe>
2023-10-17 17:00:42 -0500

    Feat(Categories): Created Category Model

---
+++ Learning Center.API03/Learning/Domain/Models/Category.cs
@@ -0,0 +1,8 @@
+namespace Learning_Center.API03.Learning.Domain.Models;
+
+public class Category
+{
+    //usaremos el patron de diseño de identity field 
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ Learning Center.API03/Learning/Domain/Models/Tutorial.cs
@@ -0,0 +1,6 @@
+namespace Learning_Center.API03.Learning.Domain.Models;
+
+public class Tutorial
+{
+    
+}
