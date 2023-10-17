56c3a2c0948d343ce7b8a1779e4d78b79f53a92a
Jaime Escobar <pcccjesc@upc.edu.pe>
2023-10-17 17:01:48 -0500

    Feat(Categories): Implemented Property-based Relationship With Tutorial

--- Learning Center.API03/Learning/Domain/Models/Category.cs
+++ Learning Center.API03/Learning/Domain/Models/Category.cs
@@ -5,4 +5,9 @@ public class Category
     //usaremos el patron de diseño de identity field 
     public int Id { get; set; }
     public string Name { get; set; }
+    //como ya existe un tutorial y como category puede abarcar varios tutoriales 
+    //debemos representar esa relacion a traves de clases 
+    
+    //de esta manera establecemos la relacion de asociacion de uno a muchos 
+    public IList<Tutorial> Tutorials { get; set; } = new List<Tutorial>();
 }
--- Learning Center.API03/Learning/Domain/Models/Tutorial.cs
+++ Learning Center.API03/Learning/Domain/Models/Tutorial.cs
@@ -2,5 +2,19 @@ namespace Learning_Center.API03.Learning.Domain.Models;
 
 public class Tutorial
 {
+    //colocamos el Id, Name Description 
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
     
+    //colocando la relacion con tutorial 
+    //aplicando RelationShip
+    //persistencia para Asp.Net core Entity Framework
+    //es necesario que se indique el atributo que sera la contraparte a nivel de estructura de tabla
+    public int CategoryId { get; set; }
+    
+    
+    
+    //aplicando POO
+    public Category Category { get; set; }
 }
